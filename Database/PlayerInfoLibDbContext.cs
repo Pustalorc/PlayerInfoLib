993a23c2895fc32ea2581aec6ecf377a698e8d5d
Pustalorc <persiafighter@gmail.com>
2021-07-21 20:26:01 +0200

    Solved DB Generation issue

--- Database/PlayerInfoLibDbContext.cs
+++ Database/PlayerInfoLibDbContext.cs
@@ -9,8 +9,8 @@ namespace Pustalorc.PlayerInfoLib.Unturned.Database
 {
     public class PlayerInfoLibDbContext : OpenModDbContext<PlayerInfoLibDbContext>
     {
-        public DbSet<Server> Servers { get; set; }
-        public DbSet<PlayerData> Players { get; set; }
+        public DbSet<Server> Servers => Set<Server>();
+        public DbSet<PlayerData> Players => Set<PlayerData>();
 
         public PlayerInfoLibDbContext(IDbContextConfigurator configurator, IServiceProvider serviceProvider) : base(configurator, serviceProvider)
         {
