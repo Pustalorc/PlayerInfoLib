af7eec0e55cae8904e45e51432710b67faf8bbe3
Nuage <77046080+nuage00@users.noreply.github.com>
2021-07-21 12:13:28 +0300

    Fixes

--- PlayerInfoLibrary.cs
+++ PlayerInfoLibrary.cs
@@ -9,65 +9,47 @@ using Pustalorc.PlayerInfoLib.Unturned.Database;
 [assembly:
 
     PluginMetadata("Pustalorc.PlayerInfoLib.Unturned", Author = "Pustalorc",
-
         DisplayName = "Player Info Library Unturned",
-
         Website = "https://github.com/Pustalorc/PlayerInfoLib/")]
 
 namespace Pustalorc.PlayerInfoLib.Unturned
-
 {
 
     public class PlayerInfoLibrary : OpenModUnturnedPlugin
-
     {
-
         private readonly ILogger<PlayerInfoLibrary> m_Logger;
 
         private readonly PlayerInfoLibDbContext m_DbContext;
 
         private readonly IPlayerInfoRepository m_PlayerInfoRepository;
 
-
         public PlayerInfoLibrary(ILogger<PlayerInfoLibrary> logger, IServiceProvider serviceProvider,
 
             PlayerInfoLibDbContext dbContext, IPlayerInfoRepository playerInfoRepository) : base(serviceProvider)
 
         {
-
             m_Logger = logger;
 
             m_DbContext = dbContext;
 
             m_PlayerInfoRepository = playerInfoRepository;
-
         }
-
 
         protected override async UniTask OnLoadAsync()
         {
-
             await m_DbContext.OpenModMigrateAsync();
-
 
             await m_PlayerInfoRepository.CheckAndRegisterCurrentServerAsync();
 
-
             m_Logger.LogInformation("Player Info Library for Unturned by Pustalorc was loaded correctly.");
-
         }
 
 
         protected override UniTask OnUnloadAsync()
         {
-
             m_Logger.LogInformation("Player Info Library for Unturned by Pustalorc was unloaded correctly.");
 
-
             return UniTask.CompletedTask;
-
         }
-
     }
-
 }
